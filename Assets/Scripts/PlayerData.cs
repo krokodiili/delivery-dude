57330749043282e5e22d3e00a0572b8c84727a54
Remssi <janne.nilsson@metropolia.fi>
2018-05-19 00:22:48 +0300

    Added base structure and movement

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour {
+
+    public float movementSpeedX = 1f;
+    public float movementSpeedY = 1f;
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public PlayerData pData;
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    // Simple movement of 8 directions, controlled with WASD
+    private void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-pData.movementSpeedX, pData.movementSpeedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(pData.movementSpeedX, pData.movementSpeedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-pData.movementSpeedX, -pData.movementSpeedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(pData.movementSpeedX, -pData.movementSpeedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, pData.movementSpeedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-pData.movementSpeedX, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -pData.movementSpeedY, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(pData.movementSpeedX, 0, 0);
+        }
+    }
+}
