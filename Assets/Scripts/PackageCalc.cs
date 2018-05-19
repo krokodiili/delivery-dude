0ac0cb98b75ff323ec7ec7db1751bab37e63b851
Lauri Meltti <laurimeltti@gmail.com>
2018-05-20 00:18:23 +0300

    added packagecalc and timer base scripts

---
+++ Assets/Scripts/PackageCalc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageCalc : MonoBehaviour {
+    public int packageDeliveryAmount;
+    int packagesDelivered = 0;
+    public Timer t;
+
+    public void deliverPackage() {
+        packagesDelivered++;
+        if(packagesDelivered >= packageDeliveryAmount) {
+            //endGame(t.timer)
+        }
+    }
+
+	private void OnGUI() {
+        GUI.Label(
+            new Rect(10, 30, 250, 100),
+            packagesDelivered + "/" + packageDeliveryAmount
+        );
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public float timer;
+    string formattedTimer;
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+
+        formattedTimer = string.Format("{0}:{1}", minutes, seconds);
+
+        print(formattedTimer);
+
+	}
+
+	private void OnGUI() {
+        GUI.Label(new Rect(10, 10, 250, 100), formattedTimer);
+	}
+}
